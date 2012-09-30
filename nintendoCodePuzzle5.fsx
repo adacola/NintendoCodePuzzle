044b04092d7666c75b6379725b5ed18910938055
adacola <adacola@le.to>
2012-09-30 21:29:26 +0900

    nintendoCodePuzzle5.fsxを追加

---
+++ nintendoCodePuzzle5.fsx
@@ -0,0 +1,9 @@
+﻿(*
+http://cp1.nintendo.co.jp/{Code Puzzle 4の次}
+*)
+#load "nintendoCodePuzzle1.fsx"
+
+open NintendoCodePuzzle1
+
+let solve() =
+    Seq.initInfinite id |> Seq.find (fun i -> lets_take_tea_break (bigint i) 1985I 33067I 84I)
